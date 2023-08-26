ee61b717d6402ab56aa078eaf9c2d3bcb736750c
vickouma77 <Vickprestone20@gmail.com>
2023-08-26 22:23:48 +0300

    app

--- Todo_List/app.js
+++ Todo_List/app.js
@@ -4,3 +4,26 @@ const express = require('express');
 const app = express();
 
 app.set("view engine", "ejs");
+
+const todos = [{
+    id: 1,
+    title: "lunch",
+},
+{
+    id: 2,
+    title: "dinner",
+},
+{
+    id: 3,
+    title: "breakfast",
+}
+]
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('index',{
+        todos: todos
+    });
+})
