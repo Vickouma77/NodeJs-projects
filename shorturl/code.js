88bc02dd37c6ffceabfe792fbc7779886f8cba06
vickouma77 <Vickprestone20@gmail.com>
2023-10-28 00:17:23 +0300

    connecting to mysql database

--- shorturl/code.js
+++ shorturl/code.js
@@ -1,11 +1,30 @@
 const express = require('express');
+const mysql = require('mysql');
 
 const app = express();
 
 app.use(express.static('public'));
 
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"shorturls"
+})
+
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log("Mysql connected");
+})
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
+})
+
+app.post('/api/create-short-url', (req, res) => {
+    let uniqueId = Math.random().toString(36).substring(2, 7);
 })
 
 app.listen(5000, () => {
