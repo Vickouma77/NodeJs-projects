7fd1855139ff47e411e2090fbe69c472f800aa2b
vickouma77 <Vickprestone20@gmail.com>
2023-10-29 22:37:04 +0300

    link to database

--- shorturl/code.js
+++ shorturl/code.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const mysql = require('mysql');
 
+require('dotenv').config();
+
+
 const app = express();
+
+const pass_word = process.env.MYSQL_PASSWORD;
 
 app.use(express.static('public'));
 
 const db = mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"",
+    password: pass_word,
     database:"shorturls"
 })
 
@@ -25,6 +30,14 @@ app.get('/', (req, res) => {
 
 app.post('/api/create-short-url', (req, res) => {
     let uniqueId = Math.random().toString(36).substring(2, 7);
+    let sql = `INSERT INTO urls (url, unique_id) VALUES ('${req.query.url}', '${uniqueId}')`;
+    let query = db.query(sql, (err, result) => {
+        if(err) throw err;
+        res.send({
+            "status": "success",
+            "short_url": `http://localhost:5000/${uniqueId}`
+        })
+    })
 })
 
 app.listen(5000, () => {
